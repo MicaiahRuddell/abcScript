5c78f5d3ef8c06ec00ae72e27e4c798c9f41cc79
MicaiahRuddell <54895722+MicaiahRuddell@users.noreply.github.com>
2022-05-30 21:48:34 -0500

    Add files via upload

---
+++ abcScript/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace abcScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool invalidInp = true;
+            string userInp;
+
+// Prompt user to input a string, and continue prompting until the user input is valid
+            while (invalidInp)
+            {
+                Console.WriteLine("Please enter your input as an alphabetical string of no greater than 256 characters");
+                userInp = Console.ReadLine();
+                if (userInp.Length <= 256)
+                {
+                    invalidInp = false;
+                    userInp = userInp.ToLower();
+
+// Using HashSet class to create an alphabet list
+                    HashSet<char> abcOutputs = new HashSet<char>();
+                    for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+                    {
+                        abcOutputs.Add(alphabet);
+                    }
+
+// Iterate through user input and remove any characters from alphabet list
+                    foreach (char c in userInp)
+                    {
+                        if (abcOutputs.Contains(c))
+                        {
+                            abcOutputs.Remove(c);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+// Output final list and list size
+                    Console.WriteLine(String.Join(",", abcOutputs));
+                    Console.WriteLine("The new alphabet list contains {0} elements: ", abcOutputs.Count);
+
+                }
+            }
+            
+            
+        }
+    }
+}
